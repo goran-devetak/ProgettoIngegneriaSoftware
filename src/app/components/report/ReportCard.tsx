e670d68cdd5802b3529067866cb3de99b13d99ab
Obamium68 <andrea.f.matera@gmail.com>
2024-12-03 16:36:42 +0100

    Half way to report

--- src/app/components/report/ReportCard.tsx
+++ src/app/components/report/ReportCard.tsx
@@ -8,27 +8,32 @@ interface ReportCardProps {
 }
 
 const ReportCardComponent: React.FC<ReportCardProps> = ({ report }) => {
-    const { description, timestamp, contacts } = report;
+    const { stationId, description, timestamp, contacts, _id , state} = report;
     return (
         <Link
-            href={`./reports/`}
+            href={`./reports/${_id}`}
             className="rounded-md w-1/2 grid grid-cols-12 bg-white shadow p-3 gap-2 items-center hover:shadow-lg transition delay-150 duration-300 ease-in-out hover:scale-105 transform"
         >
             <div className="col-span-10">
                 <div className="flex space-x-1">
-                    <p className="text-gray-600 font-bold">{description}:</p>
+                    <p className="text-black font-bold">{stationId}</p>
+                    <p className="text-gray-600 font-bold">{description}</p>
                 </div>
                 <div>
                     <p className="text-sm text-gray-800 font-light">
-                        {`${timestamp}`}
+                        {`${new Date(timestamp).toLocaleString()}`}
                     </p>
                 </div>
                 <div>
                     <div>{contacts?.email}</div>
                     <div>{contacts?.phone}</div>
                 </div>
+                <div>
+                    <p>Stato</p>
+                    {state?"Risolta":"Non risolta"}
+                </div>
             </div>
-        </Link> 
+        </Link>
     );
 };
 
--- src/app/lib/models/report/Report.ts
+++ src/app/lib/models/report/Report.ts
@@ -1,24 +1,25 @@
-import {Schema, Document} from 'mongoose';
+import { Schema, Document } from 'mongoose';
 
-export interface Report extends Document{
-    state: boolean;
-    description: string;
-    photo?: string;
-    timestamp: number;
-    contacts?: {
-      email?: string;
-      phone?: number;
-    };
-  }
-  
-  export const ReportSchema = new Schema<Report>({
-    state: { type: Boolean, required: true },
-    description: { type: String, required: true },
-    photo: { type: String, required: false},
-    timestamp:{type: Number, required:true},
-    contacts: {
-      email: { type: String },
-      phone: { type: Number },
-    },
-  });
-  
+export interface Report extends Document {
+  stationId:string;
+  state: boolean;
+  description: string;
+  photo?: string;
+  timestamp: number;
+  contacts?: {
+    email?: string;
+    phone?: number;
+  };
+}
+
+export const ReportSchema = new Schema<Report>({
+  stationId:{type:String, required:true},
+  state: { type: Boolean, required: true },
+  description: { type: String, required: true },
+  photo: { type: String, required: false },
+  timestamp: { type: Number, required: true },
+  contacts: {
+    email: { type: String, required: true },
+    phone: { type: Number, required: true },
+  },
+});
