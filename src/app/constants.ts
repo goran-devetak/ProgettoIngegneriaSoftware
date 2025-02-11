b6e76f2afde17a5da6089329df2610766e7892f5
Obamium68 <andrea.f.matera@gmail.com>
2025-02-11 20:38:52 +0100

    fixed site links

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -5,17 +5,17 @@ const COLORS = {
   red: '#dc2626',
   green: '#16a34a',
 };
-
+/*
 const URLS = {
   homepage: "http://localhost:3000/",
   apis: "http://localhost:3000/api",
-};
-/*
+};*/
+
 const URLS = {
   homepage: "https://progetto-ingegneria-software.vercel.app/",
   apis: "https://progetto-ingegneria-software.vercel.app/api",
 };
-*/
+
 
 
 export { COLORS, URLS };
